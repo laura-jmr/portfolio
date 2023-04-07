cd98f315903fbdc1999737647d1ac1a184e5a490
Laura Jürgensmeier <laura.juergensmeier@gmail.com>
2023-04-07 22:28:53 +0200

    feat: added text that follows mouse

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Title from './components/Title'
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+
 function App() {
+  const handleCanvasCreated = (canvas) => {
+    console.log(canvas.offsetWidth, canvas.offsetHeight);
+  };
+
+
   return (
-    <>
-    </>
+      <Canvas 
+      style={{ height: '100vh', width: '100vw' }}
+      onCreated={handleCanvasCreated}
+      >
+        <pointLight position={[10, 10, 10]} />
+        <Title />
+      </Canvas>
   );
 }
 
